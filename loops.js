40b4686e626d90d711fe5f9983f0aa3e0db5619c
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-15 04:32:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,6 +21,12 @@ return 'done'
 }
 
 function doWhileLoop(num){
+  var i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
   do {
     console.log("I run once regardless.")
   } while (incrementVariable() < num) {
