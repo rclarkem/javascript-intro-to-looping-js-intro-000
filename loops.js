7453b0c7bc42703892f4f9cbff9d28911e70cf30
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-15 04:13:09 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop(arr){
-let str1 = "" 
+let str1 = ""
   for(let i = 0; i <= 25; i++){
     if(i === 1){
       arr.push("I am ${i} strange loops.")
     } else {
       arr.push("I am ${i} strange loops.")
     }
-    
+
   }
 }
